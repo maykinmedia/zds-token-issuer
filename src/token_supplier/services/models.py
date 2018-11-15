45d40cfc4483413d7edff5acd9270f1e8fdf15c3
Sergei Maertens <sergei@maykinmedia.nl>
2018-11-15 21:00:15 +0100

    :sparkles: Actually register the credentials with the services

--- src/token_supplier/services/models.py
+++ src/token_supplier/services/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+import requests
+
 
 class Service(models.Model):
     label = models.CharField(_("label"), max_length=100)
@@ -9,10 +11,24 @@ class Service(models.Model):
     def __str__(self):
         return self.label
 
+    def _get_api_root(self) -> str:
+        """
+        Return the API root with guaranteed trailing slash
+        """
+        slash = "/" if not self.api_root.endswith('/') else ""
+        return f"{self.api_root}{slash}"
+
     @property
     def oas_url(self) -> str:
-        slash = "/" if not self.api_root.endswith('/') else ""
-        return f"{self.api_root}{slash}schema/openapi.yaml"
+        root = self._get_api_root()
+        return f"{root}schema/openapi.yaml"
 
     def register_client(self, client_id: str, secret: str) -> None:
-        pass
+        root = self._get_api_root()
+        endpoint = f"{root}jwtsecret/"
+
+        response = requests.post(endpoint, json={
+            'identifier': client_id,
+            'secret': secret
+        })
+        assert response.status_code == 201, "Create went wrong"
