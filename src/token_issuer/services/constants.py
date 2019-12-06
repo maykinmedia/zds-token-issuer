da9fb4396372570ea7df1f3fc731dd8e1868944c
Sergei Maertens <sergei@maykinmedia.nl>
2019-12-06 15:36:08 +0100

    :bug: Update value of constant to normalized version

--- src/token_issuer/services/constants.py
+++ src/token_issuer/services/constants.py
@@ -3,10 +3,10 @@ from djchoices import ChoiceItem, DjangoChoices
 
 class VertrouwelijkheidsAanduiding(DjangoChoices):
     openbaar = ChoiceItem('openbaar', 'OPENBAAR')
-    beperkt_openbaar = ChoiceItem('beperkt openbaar', 'BEPERKT OPENBAAR')
+    beperkt_openbaar = ChoiceItem('beperkt_openbaar', 'BEPERKT OPENBAAR')
     intern = ChoiceItem('intern', 'INTERN')
     zaakvertrouwelijk = ChoiceItem('zaakvertrouwelijk', 'ZAAKVERTROUWELIJK')
     vertrouwelijk = ChoiceItem('vertrouwelijk', 'VERTROUWELIJK')
     confidentieel = ChoiceItem('confidentieel', 'CONFIDENTIEEL')
     geheim = ChoiceItem('geheim', 'GEHEIM')
-    zeer_geheim = ChoiceItem('zeer geheim', 'ZEER GEHEIM')
+    zeer_geheim = ChoiceItem('zeer_geheim', 'ZEER GEHEIM')
